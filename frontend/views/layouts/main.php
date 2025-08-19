cfa87ebb2662a0aac74264233694fdf997d0bce7
anonymousecode <anonymousecode1@gmail.com>
2025-08-19 10:37:18 +0530

    changed favicon

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,6 +18,8 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="<?= Yii::getAlias('@web') ?>/favicon.ico">
+
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
